86e82ce415d530a46dfe65006941cb0b0c1336c4
Raffiefh <ifranraffi24@gmail.com>
2024-08-13 22:31:25 +0700

    Product Homepage

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,3 +1,11 @@
-<div>
-    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
-</div>
+<main>
+    <img src="img/Headers.png" alt="Headers" class="mt-12 w-full h-auto object-cover-1 z-1s">
+        <div class="absolute z-20 top-0 left-0 right-0 flex flex-col items-center justify-center h-full text-white">
+            <h2 class="text-primary font-inter text-3.2 tracking-wider font-medium mb-5vh mt-14.5 ">Selamat Datang di KATALOG!</h2>
+            <h2 class="text-primary font-sans text-1.6 tracking-wider font-medium">Temukan Berbagai Pilihan Gadget Terkini dan Berbagai Toko Terpercaya</h2>
+            <h2 class="text-primary font-sans text-1.6 tracking-wider font-medium">dan Jadikan Kehidupan Digitalmu Lebih Seru!</h2>
+            <button class="font-all w-22 h-11.5 bg-yellowbut text-1.9 font-semibold text-white rounded-1 cursor-pointer hover:bg-hvryellow mt-12">Selengkapnya</button>
+        </div>
+</main>
+
+
