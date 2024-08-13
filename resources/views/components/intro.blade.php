1ea0990c33d7ada3c221c535aab484834eb893bf
Raffiefh <ifranraffi24@gmail.com>
2024-08-14 02:01:28 +0700

    Intro

--- resources/views/components/intro.blade.php
+++ resources/views/components/intro.blade.php
@@ -1,14 +1,17 @@
-<div class="container mx-auto px-full py-64 bg-primary">
-        <h1>Tentang Kami</h1>
-        <div class="flex  justify-center items-center">
-            <div>
-                <img src="img/Katalog.svg" alt="People assembling puzzle pieces" class="w-1/2">
-                <img src="img/Perusahaan.svg" alt="People assembling puzzle pieces" class="w-1/2">
+<div class="container mx-auto px-full py-[6vh] bg-primary">
+    <div class="top-0 left-0 w-full h-[0.1vh] bg-gray-300"> </div>
+    <h1 class="text-secondary text-[3.2vw] mt-[4vh] font-medium tracking-wider text-center">Tentang Kami</h1>
+    <div class="mr-[10vw] ml-[10vw]">
+        <div class="flex justify-center items-center gap-[7vw]">
+            <div class="mt-[8vh]">
+                <img src="img/Katalog.svg" alt="" class="w-[22vw] ml-[2vw]">
+                <img src="img/Perusahaan.svg" alt="" class="w-[73vw]">
             </div>
-            <div class="mt-16">
-                <p class="text-gray-600 mb-4">Di KATALOG, kami membawa Anda ke dalam dunia tak terbatas teknologi. Setiap klik membawa Anda lebih dekat untuk menemukan perangkat yang sesuai dengan gaya hidup Anda.</p>
-                <p class="text-gray-600 mb-4">Dengan antarmuka yang intuitif dan pengalaman berbelanja yang menyenangkan, kami siap membantu Anda menjelajahi, membandingkan, dan memilih produk terbaik untuk kebutuhan Anda.</p>
-                <p class="text-gray-600 mb-4">Sambut era baru inovasi dengan KATALOG, tempat di mana teknologi bertemu kepuasan pelanggan.</p>
+            <div class="mt-[8vh]">
+                <p class="text-secondary text-all text-[1.6vw] mb-[4vh]">Di <b>KATALOG</b>, kami membawa Anda ke dalam dunia tak terbatas teknologi. Setiap klik membawa Anda lebih dekat untuk menemukan perangkat yang sesuai dengan gaya hidup Anda.</p>
+                <p class="text-secondary text-all text-[1.6vw] mb-[4vh]">Dengan antarmuka yang intuitif dan pengalaman berbelanja yang menyenangkan, kami siap membantu Anda <b>menjelajahi, membandingkan,</b> dan <b>memilih</b> produk terbaik untuk kebutuhan Anda.</p>
+                <p class="text-secondary text-all text-[1.6vw] mb-[4vh]">Sambut era baru inovasi dengan <b>KATALOG</b>, tempat di mana <b>teknologi bertemu kepuasan pelanggan.</b></p>
             </div>
         </div>
+    </div>
 </div>
--- resources/views/components/navigasi.blade.php
+++ resources/views/components/navigasi.blade.php
@@ -1,15 +1,15 @@
-<nav class="w-full h-19 bg-primary flex items-center fixed top-0 left-0 z-50 shadow-sm">
-    <img src="img/Katalog.svg" alt="Katalog" class="ml-8% w-15% mr-17">
-    <ul class="list-none flex gap-1.9 mr-3.6">
-        <li><a href="/homepage" class="{{ request()->is('homepage') ? 'font-bold font-sans text-1.7' : 'font-all'}} text-secondary no-underline text-1.6">Beranda</a></li>
-        <li><a href="/product" class="{{ request()->is('product') ? 'font-bold font-sans text-1.7' : 'font-all'}} text-secondary no-underline text-1.6">Produk</a></li>
-        <li><a href="/contact" class="{{ request()->is('contact') ? 'font-bold font-sans text-1.7' : 'font-all'}} text-secondary no-underline text-1.6">Kontak</a></li>
+<nav class="w-full h-[19vh] bg-primary flex items-center fixed top-0 left-0 z-50 shadow-sm">
+    <img src="img/Katalog.svg" alt="Katalog" class="ml-[8%] w-[15%] mr-[17vw]">
+    <ul class="list-none flex gap-[1.9vw] mr-[3.6vw]">
+        <li><a href="/homepage" class="{{ request()->is('homepage') ? 'font-bold font-sans text-[1.7vw]' : 'font-all'}} text-secondary no-underline text-[1.6vw]">Beranda</a></li>
+        <li><a href="/product" class="{{ request()->is('product') ? 'font-bold font-sans text-[1.7vw]' : 'font-all'}} text-secondary no-underline text-[1.6vw]">Produk</a></li>
+        <li><a href="/contact" class="{{ request()->is('contact') ? 'font-bold font-sans text-[1.7vw]' : 'font-all'}} text-secondary no-underline text-[1.6vw]">Kontak</a></li>
     </ul>
-    <div class="flex mr-3.6 mr-8%">
-        <form class="flex mr-0.5">
-            <input class=" outline-none h-8 pl-1 pr-5.5 rounded-1 border text-inter border-greys bg-transparent transition ease-in-out duration-80 z-50 text-placenav cursor-pointer text-1 font-all tracking-wide" type="text" placeholder="Cari produk pilihanmu">
-            <img class="absolute w-1.7 mt-2 ml-17.2 mr-9%"src="img/Search.svg" alt="Search">
+    <div class="flex mr-[3.6vw] mr-8%">
+        <form class="flex mr-[0.5vw]">
+            <input class=" outline-none h-[8vh] pl-[1vw] pr-[5.5vw] rounded-1 border text-inter border-greys bg-transparent transition ease-in-out duration-80 z-50 text-placenav cursor-pointer text-1 font-all tracking-wide" type="text" placeholder="Cari produk pilihanmu">
+            <img class="absolute w-[1.7vw] mt-[2vh] ml-17.2 mr-9%"src="img/Search.svg" alt="Search">
         </form>
-        <button class="font-inter tracking-wide w-8 rounded-1 h-8 bg-secondary text-1.7 text-white cursor-pointer hover:bg-seconhvr">Masuk</button>
+        <button class="font-inter tracking-wide w-[8vw] rounded-1 h-[8vh] bg-secondary text-[1.7vw] text-white cursor-pointer hover:bg-seconhvr">Masuk</button>
     </div>
 </nav>
