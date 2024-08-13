97991ee432be1ca071e9d0f3adff873f9ab2da08
Syahrial Rafky <syahrafky08@gmail.com>
2024-08-14 00:09:28 +0700

    some changes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -14,29 +14,29 @@
     <!-- Container -->
     <div class="relative bg-blue-500 w-full max-w-lg rounded-lg shadow-lg p-8">
         
-        <!-- Contact Us Text -->
+        <!-- Contact Us -->
         <div class="absolute top-0 left-0 mt-4 ml-4 text-white text-lg font-semibold">
         Contact Us
         </div>
 
-        <!-- Email Button -->
+        <!-- Email -->
         <div class="absolute bottom-12 left-0 mt-8 ml-7">
             <a href="mailto:someone@example.com" class="inline-block bg-white text-blue-500 font-semibold py-2 px-4 rounded-lg shadow-md">
             <i class="fas fa-envelope mr-2"></i> Email
             </a>
         </div>
 
-        <!-- WhatsApp Button -->
+        <!-- WhatsApp -->
         <div class="absolute bottom-12 right-0 mt-8 mr-7">
             <a href="https://wa.me/1234567890" class="inline-block bg-white text-green-500 font-semibold py-2 px-4 rounded-lg shadow-md">
             <i class="fab fa-whatsapp mr-2"></i> WhatsApp
             </a>
         </div>
 
-        <!-- White Circle -->
+        <!-- Lingkarann -->
         <div class="absolute right-0 top-0 h-32 w-32 rounded-full bg-white opacity-50 mr-4 mt-4"></div>
 
-        <!-- Footer Text -->
+        <!-- Footer -->
         <div class="absolute bottom-0 left-0 mb-4 ml-4 text-white text-sm">
             2024 UKM Linux and Open Source
         </div>
