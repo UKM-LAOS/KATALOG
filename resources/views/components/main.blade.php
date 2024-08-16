1fb592159b5af23814e25ce46da883d721d90404
Phisict <adrianfathan69@gmail.com>
2024-08-16 22:20:42 +0700

    tipis tipis fix

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,11 +1,13 @@
-<main>
-    <img src="img/Headers.png" alt="Headers" class="mt-[12vh] w-full h-auto object-cover-1 z-1s">
-        <div class="absolute z-20 top-0 left-0 right-0 flex flex-col items-center justify-center h-full text-white">
-            <h2 class="text-primary font-inter text-[3.2vw] tracking-wider font-medium mb-[5vh] mt-[14.5vh] ">Selamat Datang di KATALOG!</h2>
-            <h2 class="text-primary font-sans text-[1.6vw] tracking-wider font-medium">Temukan Berbagai Pilihan Gadget Terkini dan Berbagai Toko Terpercaya</h2>
-            <h2 class="text-primary font-sans text-[1.6vw] tracking-wider font-medium">dan Jadikan Kehidupan Digitalmu Lebih Seru!</h2>
-            <button class="font-all w-[22vw] h-[11.5vh] bg-yellowbut text-[1.9vw] font-semibold text-white rounded-1 cursor-pointer hover:bg-hvryellow mt-[12vh]">Selengkapnya</button>
-        </div>
+<main class="relative w-full h-screen">
+    <img src="img/Headers.png" alt="Headers" class="w-full h-full object-cover">
+    
+    <div class="absolute z-20 inset-0 flex flex-col items-center justify-center text-white">
+        <h2 class="text-primary font-inter text-[3.2vw] tracking-wider font-medium mb-[5vh]">Selamat Datang di KATALOG!</h2>
+        <h2 class="text-primary font-sans text-[1.6vw] tracking-wider font-medium">Temukan Berbagai Pilihan Gadget Terkini dan Berbagai Toko Terpercaya</h2>
+        <h2 class="text-primary font-sans text-[1.6vw] tracking-wider font-medium mb-[5vh]">dan Jadikan Kehidupan Digitalmu Lebih Seru!</h2>
+        <button class="font-all w-[18rem] h-[5rem] text-xl bg-yellowbut text-[1.9vw] font-semibold text-white rounded-1 cursor-pointer hover:bg-hvryellow">Selengkapnya</button>
+    </div>
 </main>
+
 
 
--- resources/views/components/navigasi.blade.php
+++ resources/views/components/navigasi.blade.php
@@ -1,15 +1,17 @@
-<nav class="w-full h-[19vh] bg-primary flex items-center fixed top-0 left-0 z-50 shadow-sm">
-    <img src="img/Katalog.svg" alt="Katalog" class="ml-[8%] w-[15%] mr-[17vw]">
-    <ul class="list-none flex gap-[1.9vw] mr-[3.6vw]">
-        <li><a href="/homepage" class="{{ request()->is('homepage') ? 'font-bold font-sans text-[1.7vw]' : 'font-all'}} text-secondary no-underline text-[1.6vw]">Beranda</a></li>
-        <li><a href="/product" class="{{ request()->is('product') ? 'font-bold font-sans text-[1.7vw]' : 'font-all'}} text-secondary no-underline text-[1.6vw]">Produk</a></li>
-        <li><a href="/contact" class="{{ request()->is('contact') ? 'font-bold font-sans text-[1.7vw]' : 'font-all'}} text-secondary no-underline text-[1.6vw]">Kontak</a></li>
+<nav class="w-full h-16 bg-primary flex items-center fixed top-0 left-0 z-50 shadow-sm px-6">
+    <img src="img/Katalog.svg" alt="Katalog" class="w-24 mr-auto">
+    
+    <ul class="list-none flex gap-6 mr-8">
+        <li><a href="/homepage" class="{{ request()->is('homepage') ? 'font-bold font-sans text-lg' : 'font-all'}} text-secondary no-underline text-base">Beranda</a></li>
+        <li><a href="/product" class="{{ request()->is('product') ? 'font-bold font-sans text-lg' : 'font-all'}} text-secondary no-underline text-base">Produk</a></li>
+        <li><a href="/contact" class="{{ request()->is('contact') ? 'font-bold font-sans text-lg' : 'font-all'}} text-secondary no-underline text-base">Kontak</a></li>
     </ul>
-    <div class="flex mr-[3.6vw] mr-8%">
-        <form class="flex mr-[0.5vw]">
-            <input class=" outline-none h-[8vh] pl-[1vw] pr-[5.5vw] rounded-1 border text-inter border-greys bg-transparent transition ease-in-out duration-80 z-50 text-placenav cursor-pointer text-1 font-all tracking-wide" type="text" placeholder="Cari produk pilihanmu">
-            <img class="absolute w-[1.7vw] mt-[2vh] ml-17.2 mr-9%"src="img/Search.svg" alt="Search">
+    
+    <div class="flex items-center">
+        <form class="relative flex items-center">
+            <input class="outline-none h-10 pl-4 pr-12 rounded border text-sm border-greys bg-transparent transition duration-150 ease-in-out text-placenav cursor-pointer" type="text" placeholder="Cari produk pilihanmu">
+            <img class="absolute w-[2rem] right-3 top-1/2 transform -translate-y-1/2" src="img/Search.svg" alt="Search">
         </form>
-        <button class="font-inter tracking-wide w-[8vw] rounded-1 h-[8vh] bg-secondary text-[1.7vw] text-white cursor-pointer hover:bg-seconhvr">Masuk</button>
+        <button class="font-inter tracking-wide px-4 py-2 ml-4 rounded-lg bg-secondary text-white text-base hover:bg-seconhvr">Masuk</button>
     </div>
 </nav>
