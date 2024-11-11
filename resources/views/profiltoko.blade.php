24036205d42c1e1d171567a2050b4229363a6cfe
ForceClose31 <forceclose31@gmail.com>
2024-11-11 20:54:36 +0700

    fix conflict

--- resources/views/profiltoko.blade.php
+++ resources/views/profiltoko.blade.php
@@ -3,30 +3,16 @@
 @section('title', 'Profil Admin')
 
 @section('content')
-@if(session('success'))
-<script>
-    Swal.fire({
-        icon: 'success',
-        title: 'Sukses!',
-        text: '{{ session('success') }}',
-        timer: 2000, 
-        showConfirmButton: false
-    });
-</script>
-@endif
     <div class=" bg-white rounded-lg px-3 py-2 shadow-md mx-auto">
-        <div class="flex flex-col">
-            <div class="flex items-center justify-between mb-6">
-            <h1 class="text-left mt-2 font-semibold text-4xl mb-6">{{$user->toko->namatoko}}</h1>
-            <button class="align-center font-semibold w-32 h-[32] text-[2] bg-red-600 text-red-50 rounded-1 cursor-pointer hover:bg-red-500 ml-44 mb-4"><a href="{{route('logout')}}">Logout</a></button>
-            </div>
-            <h1 class="text-left text-1xl"><b>Email</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp: &nbsp {{$user->email}}</h1>
+        <d class="flex flex-col">
+            <h1 class="text-left mt-2 font-semibold text-4xl mb-6">Axio Shop</h1>
+            <h1 class="text-left text-1xl"><b>Email</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp: &nbsp Xauxau@gmail.com</h1>
             <br>
             <div class="flex gap-4 mb-4 ">
-                <img src="{{ asset('storage/' . $user->toko->fototoko) }}" alt="LAOS MUDA TIDAK MENYERAH LAOS JAYA" class="w-72">
+                <img src="/img/TOKO_KOMPUTER.jpeg" alt="LAOS MUDA TIDAK MENYERAH LAOS JAYA" class="w-72">
                 <div class=" bg-bg-zinc-400 max-w-[784] shadow-md mx-auto">
                     <h1 class="mt-[20] text-xl ml-4 font-semibold mb-[12]">Deskripsi Toko</h1>
-                    <p class="ml-4">{{$user->toko->deskripsitoko}}</p>
+                    <p class="ml-4">Axio Shop sendiri merupakan toko reparasi dan penjualan kebutuhan komputer, Axio Shop menawarkan berbagai kebutuhan dari perbaikan atau reparasi, kebutuhan komputer, handphone, kabel dan sebaginya. <br> Axio Shop: Perbaikan Cepat, Hasil Maksimal Bosan menunggu lama untuk perbaikan gadget? Pilih Axio Shop! Kami berkomitmen untuk menyelesaikan perbaikan secepat mungkin tanpa mengorbankan kualitas. Nikmati layanan antar-jemput gratis dan garansi untuk setiap perbaikan.</p>
                 </div>
             </div>
                 <div class="flex ml-36">
@@ -45,7 +31,7 @@
             <h2 class="text-xl font-semibold mb-4">Edit Profil</h2>
 
             <!-- Form Update Profil -->
-            <form action="{{route('profilTokoEdit', $user->id)}}" method="post" enctype="multipart/form-data">
+            {{-- <form action="{{route('profiladmin.update', $user->id)}}" method="post" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
 
@@ -56,6 +42,7 @@
                         class="w-full border border-gray-300 p-2 rounded-md @error('email') border-red-500 @enderror"
                         value="{{ old('email', $user->email) }}" required>
 
+                    <!-- Error for Email -->
                     @error('email')
                     <div class="text-red-500 text-sm mt-2">
                         {{ $message }}
@@ -70,6 +57,7 @@
                         class="w-full border border-gray-300 p-2 rounded-md @error('password') border-red-500 @enderror"
                         oninput="toggleKonfirmPassword()">
 
+                    <!-- Error for Password -->
                     @error('password')
                     <div class="text-red-500 text-sm mt-2">
                         {{ $message }}
@@ -83,70 +71,12 @@
                     <input type="password" id="password_confirmation" name="password_confirmation"
                         class="w-full border border-gray-300 p-2 rounded-md" disabled>
                 </div>
-                 <!-- Nama Toko -->
-                 <div class="mb-4">
-                    <label for="namatoko" class="block text-gray-700 font-semibold mb-2">Nama Toko</label>
-                    <input type="text" id="namatoko" name="namatoko"
-                        class="w-full border border-gray-300 p-2 rounded-md @error('namatoko') border-red-500 @enderror"
-                        value="{{ old('namatoko', $user->toko->namatoko) }}" required>
 
-                    @error('namatoko')
-                    <div class="text-red-500 text-sm mt-2">
-                        {{ $message }}
-                    </div>
-                    @enderror
-                </div>
-
-                 <!-- Input Foto Toko -->
-                 <div class="mb-4">
-                    <label for="fototoko" class="block text-gray-700 font-semibold mb-2">Foto Toko</label>
-                    <input type="file" name="fototoko" id="fototoko" />
-                    @if($user->toko->fototoko)
-                    <img src="{{ asset('storage/' . $user->toko->fototoko) }}" alt="Gambar Tempat" class="mt-2" width="150">
-                    @endif
-                    {{-- <input type="file" id="fototoko" name="fototoko"
-                        class="w-full border border-gray-300 p-2 rounded-md @error('fototoko') border-red-500 @enderror"
-                        value="{{ old('fototoko', $user->toko->fototoko) }}" required> --}}
-
-                    @error('fototoko')
-                    <div class="text-red-500 text-sm mt-2">
-                        {{ $message }}
-                    </div>
-                    @enderror
-                </div>
-
-                 <!-- Input Link Toko -->
-                 <div class="mb-4">
-                    <label for="linktoko" class="block text-gray-700 font-semibold mb-2">Link Toko</label>
-                    <input type="text" id="linktoko" name="linktoko"
-                        class="w-full border border-gray-300 p-2 rounded-md @error('linktoko') border-red-500 @enderror"
-                        value="{{ old('linktoko', $user->toko->linktoko) }}" required>
-
-                    @error('linktoko')
-                    <div class="text-red-500 text-sm mt-2">
-                        {{ $message }}
-                    </div>
-                    @enderror
-                </div>
-
-                <!--Input Deskripsi Toko -->
-                <div class="mb-4">
-                    <label for="deskripsitoko" class="block text-gray-700 font-semibold mb-2">Deskripsi Toko</label>
-                    <textarea type="text" id="deskripsitoko" name="deskripsitoko"
-                        class="w-full border border-gray-300 p-2 rounded-md" cols="30" rows="10"
-                      required>{{ old('deskripsitoko', $user->toko->deskripsitoko) }} 
-                    </textarea>
-                    @error('deskripsitoko')
-                    <div class="text-red-500 text-sm mt-2">
-                        {{ $message }}
-                    </div>
-                    @enderror
-                </div>
                 <!-- Tombol Simpan -->
                 <div class="flex justify-end">
                     <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-lg">Simpan</button>
                 </div>
-            </form>
+            </form> --}}
         </div>
     </div>
 @endsection
